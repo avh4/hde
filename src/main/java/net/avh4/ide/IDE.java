0431ceb567930df50b67ce5c3713657cb042b5d1
Aaron VonderHaar <gruen0aermel@gmail.com>
2011-02-25 22:02:59 -0800

    Writing code: Java Hello World exectues

---
+++ src/main/java/net/avh4/ide/IDE.java
@@ -0,0 +1,20 @@
+package net.avh4.ide;
+
+import java.util.Set;
+
+public class IDE {
+
+	public void performUserCommand(String command, String text) {
+	}
+	
+	public void enterText(String text) {
+	}
+	
+	public Set<BuildError> getErrors() {
+		return null;
+	}
+	
+	public Set<BuildWarning> getWarnings() {
+		return null;
+	}
+}
